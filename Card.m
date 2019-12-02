b834ead3f9bf56ed1329c0ad5584d6a1905d358f
hhan68 <57933378+hhan68@users.noreply.github.com>
2019-12-02 00:35:15 -0800

    Update Card.m

--- Card.m
+++ Card.m
@@ -5,15 +5,15 @@ classdef Card
         privacy = 0; %0 = facedown, 1 = faceup
     end
     methods
-        function card = Card(suit, value, privacy)
+        function obj = Card(suit, value, privacy)
             if nargin == 3
-                card.suit = suit;
-                card.value = value;
-                card.privacy = privacy;
+                obj.suit = suit;
+                obj.value = value;
+                obj.privacy = privacy;
             else
-                card.suit = [];
-                card.value = [];
-                card.privacy = [];
+                obj.suit = [];
+                obj.value = [];
+                obj.privacy = [];
             end
         end
     end
