4b7de1c9db37b11027e61bd4ce853d7d904533b0
ADPatkar <58122346+ADPatkar@users.noreply.github.com>
2019-12-03 14:33:01 -0800

    Update Deck.m

--- Deck.m
+++ Deck.m
@@ -4,13 +4,14 @@ classdef Deck
     end
     methods
         function obj = Deck()
-            import pkg.Card.*
-            for i = ["Hearts", "Clubs", "Diamonds", "Spades"]
+            import pkg.Card.* %import class definition of Card
+            for i = ["Hearts", "Clubs", "Diamonds", "Spades"] 
                 for j = [2:10, "J", "Q", "K", "A"]
-                    x = Card(i, j, 0);
-                    obj.cards = [obj.cards, x];
+                    x = Card(i, j, 0); %initalize every card in deck
+                    obj.cards = [obj.cards, x]; %add to array Deck
                 end
             end
+            obj.cards= obj.cards(randperm(52)) %shuffle deck
         end
     end
 end
